57cf9c97efbaef08678fa0a0e4e4595e1a26f48b
Arturo Vaine <arturo.vaine@gmail.com>
2021-07-27 20:11:25 -0300

    Requisito 4 ok

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -34,7 +34,15 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.strictEqual(circle(2).circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.strictEqual(parseInt(parseFloat(circle(3).area).toPrecision(2), 10), 28); // Arredondamento da área com parseInt. Fonte: https://eslint.org/docs/rules/radix
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    const radius = parseInt((circle(3).radius).toPrecision(2), 10);
+    const area = parseInt((circle(3).area).toPrecision(2), 10);
+    const circumference = parseInt((circle(3).circumference).toPrecision(2), 10);
+    assert.strictEqual(radius, 3);
+    assert.strictEqual(area, 28);
+    assert.strictEqual(circumference, 19);
   });
 });
