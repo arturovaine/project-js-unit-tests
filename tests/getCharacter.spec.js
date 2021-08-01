9f4bc84e66d0f028ddc69050a059611854014ed6
Arturo Vaine <arturo.vaine@gmail.com>
2021-08-01 15:39:07 -0300

    getChar

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -46,13 +46,36 @@ OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enq
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.deepStrictEqual(getCharacter(), undefined);    
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    const objArya = {
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: ['Not today', 'A girl has no name.'],
+    };
+    assert.deepStrictEqual(getCharacter('Arya', objArya));
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    const objBrienne = {
+      name: 'Brienne Tarth',
+      class: 'Knight',
+      phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'],
+    };
+    assert.deepStrictEqual(getCharacter('Brienne', objBrienne));
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    const objMelissandre = {
+      name: 'Melissandre',
+      class: 'Necromancer',
+      phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'],
+    };
+    assert.deepStrictEqual(getCharacter('Melissandre', objMelissandre));
     // Teste se a função se os parâmetros não são Case Sensitive.
+    assert.deepStrictEqual(getCharacter('ARYA', objArya));
+    // assert.deepStrictEqual(getCharacter('BRIENNE', objBrienne));
+    // assert.deepStrictEqual(getCharacter('MELISSANDRE', objMelissandre));
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    assert.deepStrictEqual(getCharacter('SirFulanoDoReinoDeTal', undefined));
   });
 });
